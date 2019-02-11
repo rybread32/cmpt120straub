45c5dac8b1e07cfa6e5298b71750795e378e7f25
Ryan Straub <ryan.straub2@marist.edu>
2019-02-11 18:05:33 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#madlib.py
+#   A program used for making a madlib
+#by: Ryan Straub
+
+def main():
+    noun = input("Please insert a noun.")
+    verb = input("Please insert a verb.")
+    adjective = input("Please insert an adjective.")
+    place = input("please insert a place.")
+    print("Take your", adjective, noun, "and", verb, "it at the", place,"!")
+
+main()
