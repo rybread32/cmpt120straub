255a982086deef79eede065549f5e9a1b47ae597
Ryan Straub <ryan.straub2@marist.edu>
2019-02-11 17:23:30 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,14 @@
+#rover.py
+#   A program to determine how fast light travels from Mars to Earth
+#by Ryan Straub
+
+def main():
+    Speed = 186000
+    Distance = 34000000
+    Time = Distance / Speed
+    print("The time it takes is", Time, "seconds.")
+
+main()
+    
+
+    
