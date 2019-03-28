6900e2e25b84b6f1c8443f4ac29ef590888e1006
Ryan <ryan.straub.marist@gmail.com>
2019-03-28 15:18:47 -0400

    Usability improvements to the guessing game, resolves #3 and #4 and #5.

--- guessing-game.py
+++ guessing-game.py
@@ -5,12 +5,16 @@ def main():
         animal = "dog"
         answer = input("I'm thinking of an animal that is known as 'Man's best friend': ").lower()
         if answer == animal:
-            print("Correct!")
-            break
-        elif answer == "quit":
+            opinion = input("Correct! Do you like this animal?(y/n) ").lower()
+            if opinion == "y":
+                print("Great!")
+                break
+            elif opinion == "n":
+                print("Oh no!")
+                break
+        elif answer[0] == "q":
             break
         else:
             print("Thats not it. Try again!")
                 
 main()
-    
