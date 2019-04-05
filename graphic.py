a2497ecb02cfd2b26c8f48adea33ff6a989de567
Ryan <ryan.straub.marist@gmail.com>
2019-04-05 17:06:10 -0400

    Created main GUI for calc_function.py. Still have to fix clear button and negative button.

---
+++ graphic.py
@@ -0,0 +1,77 @@
+#graphics.py
+#Graphical User Interface(GUI) for the calc_functions program
+#Created by Ryan Straub
+#4/5/19
+
+
+from graphics import *
+
+from calc_functions import *
+
+def createCanvas():
+    win = GraphWin("Calculator",400, 400)
+    win.setBackground("Blue")
+    return win
+
+def createDisplay(win):
+    rectangle =Rectangle(Point(350,25),Point(25,75)).draw(win)
+    colorscreen = "white"
+    rectangle.setFill(colorscreen)
+    size = [100]
+    text = Text(Point(250,50),"")
+    text.draw(win)
+    return text
+
+def getCoords(i):
+    coords = [[50,275],[200,335],[125,275],[50,215],[125,215],[200,215],[50,155],[125,155],[200,155],[50,95],
+              [125,95],[200,95],[200,275],[275,215],[275,275],[275,155],[275,95],[275,335]]
+    return coords[i][0], coords[i][1]
+
+def getLabel(i):
+     labels = ['+/-','Clear','0','1','2','3','4','5','6','7','8','9','.','+','-','*','/','=']
+     return labels[i] 
+    
+def createButtons(win):
+    color = ['white','white','orange','orange','orange','orange','orange','orange','orange','orange','orange','orange','white','white','white','white','white','white']
+    size = [50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50]
+
+    for i in range(18):
+        x, y = getCoords(i)
+        button = Rectangle(Point(x,y),Point(x+size[i],y+size[i]))
+        button.setFill(color[i])
+        label = Text(Point(x+size[i]/2,y+size[i]/2),getLabel(i))
+        button.draw(win)
+        label.draw(win)
+
+def evaluate(mouse, display):
+    for i in range(18):
+        x,y = getCoords(i)
+        if x<mouse.x < x+50 and y< mouse.y < y+50:
+            print(getLabel(i))
+            display.setText(display.getText() + " " + getLabel(i))
+            break
+    return getLabel(i)
+
+def addEquation(equation,label):
+    if label in ['+','-','*','/']:
+        return equation + " " + label + " "
+    else:
+        return equation + label
+        
+def main():
+    win = createCanvas()
+    display = createDisplay(win)
+    equation = ""
+    coords = createButtons(win)
+    while True:
+        mouse = win.getMouse()
+        label = evaluate(mouse, display)
+        if label == "=":
+            result = pemdas(equation.split())
+            display.setText(str(result))
+        if label == "Clear":
+            del result
+        else:
+            equation = addEquation(equation, label)
+
+main()
